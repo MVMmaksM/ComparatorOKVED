35153a3b60bd5df5b5a5ece2e1364e7c6345f5a2
Maks <Maks@DESKTOP-6KU2K6N>
2022-12-02 18:14:44 +0500

    Измена VodelPBD

--- Comparator.OKVED/Model/ModelPBD.cs
+++ Comparator.OKVED/Model/ModelPBD.cs
@@ -8,13 +8,18 @@ namespace Comparator.OKVED.Model
 {
     class ModelPBD
     {
+        public string Period { get; set; }
         public string OKATO { get; set; }
         public string OKPO { get; set; }
         public string Name { get; set; }
+        public string KodPokaz { get; set; }
         public string OKVEDHoz { get; set; }
         public string OKVEDChist { get; set; }
         public double? OtchMes { get; set; }
-        public double? PerNachOtchGod { get; set; }
+        public double? PredMes { get; set; }
+        public double? SovMesPrGod { get; set; }
         public double? OtchKvart { get; set; }
+        public double? PredKvart { get; set; }
+        public double? SovKvartPrGod { get; set; }
     }
 }
